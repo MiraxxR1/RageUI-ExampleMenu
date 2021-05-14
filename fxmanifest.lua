36331ec9a682683a4c798bc9dd8bf4d2149932e1
MiraxxR1 <75571297+MiraxxR1@users.noreply.github.com>
2021-05-14 18:44:30 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+
+client_scripts {
+    "@Alvys/modules/Utils/sh_shared.lua",
+    "@cl_rui.lua",
+    '@Alvys/modules/RageUI/RMenu.lua',
+    '@Alvys/modules/RageUI/menu/RageUI.lua',
+    '@Alvys/modules/RageUI/menu/Menu.lua',
+    '@Alvys/modules/RageUI/menu/MenuController.lua',
+    '@Alvys/modules/RageUI/components/*.lua',
+    '@Alvys/modules/RageUI/menu/elements/*.lua',
+    '@Alvys/modules/RageUI/menu/items/*.lua',
+    '@Alvys/modules/RageUI/menu/panels/*.lua',
+    '@Alvys/modules/RageUI/menu/windows/*.lua',
+}

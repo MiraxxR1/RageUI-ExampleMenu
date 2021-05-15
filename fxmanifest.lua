a2c47ef422838245c0f718c5be1d605fb9d00b0e
MiraxxR1 <75571297+MiraxxR1@users.noreply.github.com>
2021-05-15 17:02:45 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,14 @@
 fx_version 'adamant'
 game 'gta5'
 
+name 'RageUI-TemplateMenu'
+author 'MiraxxR'
+contact 'MiraxxR#7492'
+repository 'https://github.com/MiraxxR1/RageUI-TemplateMenu'
+description 'It is a menu that logically does not consume since it does not run all the time. You can take the code as an example. The resource is not plug n play you will need to change the fxmanifest.lua and consumes 0.01ms. Discord: https://discord.gg/tR4f4af4Mt'
+
+dependencies {'RageUI'}
+
 client_scripts {
     "@Alvys/modules/Utils/sh_shared.lua",
     '@Alvys/modules/RageUI/RMenu.lua',

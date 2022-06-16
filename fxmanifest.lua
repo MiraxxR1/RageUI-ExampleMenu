6b61d0f832946a96aa1bf44e11d863583d815bf9
MiraxxR <75571297+MiraxxR1@users.noreply.github.com>
2022-06-16 11:20:51 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'adamant'
 game 'gta5'
 
-name 'RageUI-TemplateMenu'
+name 'RageUI-ExampleMenu'
 author 'MiraxxR'
 contact 'MiraxxR#7492'
 repository 'https://github.com/MiraxxR1/RageUI-TemplateMenu'
@@ -10,15 +10,5 @@ description 'It is a menu that logically does not consume since it does not run 
 dependencies {'RageUI'}
 
 client_scripts {
-    "@Alvys/modules/Utils/sh_shared.lua",
-    '@Alvys/modules/RageUI/RMenu.lua',
-    '@Alvys/modules/RageUI/menu/RageUI.lua',
-    '@Alvys/modules/RageUI/menu/Menu.lua',
-    '@Alvys/modules/RageUI/menu/MenuController.lua',
-    '@Alvys/modules/RageUI/components/*.lua',
-    '@Alvys/modules/RageUI/menu/elements/*.lua',
-    '@Alvys/modules/RageUI/menu/items/*.lua',
-    '@Alvys/modules/RageUI/menu/panels/*.lua',
-    '@Alvys/modules/RageUI/menu/windows/*.lua',
     "cl_rui.lua"
 }
